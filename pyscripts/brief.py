4211b0a9ec60a67cadf8c2b5afb04e0f6df5e7e1
Gabe Banks <gabe@gabebanks.net>
2023-06-05 16:44:58 -1000

    change actual brief script

--- pyscripts/brief.py
+++ pyscripts/brief.py
@@ -1,14 +1,13 @@
 import sys
 from nltk import tokenize
 import chromadb
-import time
+import openai
+import os
+from dotenv import load_dotenv
 
-# Function to print the runtime in milliseconds
-def print_runtime(start_time, end_time):
-    runtime_ms = (end_time - start_time) * 1000
-    print("Runtime:", runtime_ms, "milliseconds")
-start_time = time.time()
+load_dotenv()
 
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 if len(sys.argv) != 4:
     print("Usage: python script.py <textfile> <questionfile> <k>")
@@ -41,8 +40,6 @@ text = open(textfile, 'r').read()
 question = open(questionfile, 'r').read()
 
 long_sentences = tokenize.sent_tokenize(text)
-print("tokenized text")
-print_runtime(start_time, time.time())
 
 sentences = []
 for sentence in long_sentences:
@@ -52,34 +49,34 @@ sentences = list(filter(lambda x: len(x) > 20, sentences))
 
 chroma_client = chromadb.Client()
 
-# import openai
-# openai.api_key = "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"
-# def embedder(list_of_sentences):
-#     raw = openai.Embedding.create(
-#         model="text-embedding-ada-002",
-#         input=list_of_sentences
-#     )
-#     return list(map(lambda x: x.embedding, raw.data))
+def embedder(list_of_sentences):
+    raw = openai.Embedding.create(
+        model="text-embedding-ada-002",
+        input=list_of_sentences
+    )
+    return list(map(lambda x: x.embedding, raw.data))
 
-
-from sentence_transformers import SentenceTransformer
-model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
-embedder = lambda x: model.encode(x).tolist()
-
+current_sentences = []
+vectors = []
+# Split into chunks of 500
+for i in sentences:
+    current_sentences.append(i)
+    if len(current_sentences) >= 500 or i == sentences[-1]:
+        new_vectors = embedder(current_sentences)
+        vectors += new_vectors
+        current_sentences = []
 
 collection = chroma_client.create_collection(
-    name="my_collection", embedding_function=embedder)
+    name="my_collection", embedding_function=None)
 
 collection.add(
+    embeddings=vectors,
     documents=sentences,
-    ids=list(map(lambda x: str(x), range(len(sentences))))
+    ids=list(map(lambda x: str(x), range(len(sentences)))),
 )
 
-print("added to collection")
-print_runtime(start_time, time.time())
-
 results = collection.query(
-    query_texts=[question],
+    query_embeddings=embedder(question),
     n_results=k
 )
 

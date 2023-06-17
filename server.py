16f345894a22ccf501e05ef8ac8ed3989388d766
Kartik Donepudi <kartik@quillai.co>
2023-06-17 16:41:22 -0700

    Co-authored-by: Gabe Banks <Gbox4@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,16 +1,75 @@
-from flask import Flask, request
 from flask_cors import CORS
+from flask import Flask, request, jsonify
 from PyPDF2 import PdfReader
+import docx
 import io
+
 from bs4 import BeautifulSoup
 import requests
+from nltk import tokenize
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
+# Give sentence split function a better name, returns a list of strings
+split_into_sentences = tokenize.sent_tokenize
+
+
+ALLOWED_EXTENSIONS = {'pdf', 'txt', 'docx'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def read_pdf(file):
+    pdf_file_obj = io.BytesIO(file.read())
+    pdf_reader = PdfReader(pdf_file_obj)
+    text = ""
+    for page_num in range(len(pdf_reader.pages)):
+        page_obj = pdf_reader.pages[page_num]
+        text += page_obj.extract_text() + "\n"
+    return text.strip()
+
+def read_txt(file):
+    return file.read().decode('utf-8')
+
+def read_docx(file):
+    doc = docx.Document(io.BytesIO(file.read()))
+    full_text = []
+    for paragraph in doc.paragraphs:
+        full_text.append(paragraph.text)
+    return '\n'.join(full_text)
+
+@app.route('/upload-and-index-text', methods=['POST'])
+def upload_and_index_text():
+    if 'files[]' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+
+    files = request.files.getlist('files[]')
+    response = []
+
+    for file in files:
+        if file and allowed_file(file.filename):
+            extension = file.filename.rsplit('.', 1)[1].lower()
+
+            if extension == 'pdf':
+                content = read_pdf(file)
+            elif extension == 'txt':
+                content = read_txt(file)
+            elif extension == 'docx':
+                content = read_docx(file)
+            else:
+                return jsonify({"error": "Unsupported file type"}), 400
+
+            response.append({'filename': file.filename, 'content': content})
+
+    print(response)
+    return jsonify(response)
+
+
 
 @app.route('/extract-text-from-pdf', methods=['POST'])
-def extract_text():
+def extract_text_from_pdf():
     file = request.files['file']
     if file:
         pdf_file_obj = io.BytesIO(file.read())
